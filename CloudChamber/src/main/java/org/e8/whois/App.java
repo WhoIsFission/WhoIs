e6a69b3c8e62415f210f9236f1925942c283bf9f
Abhijit <abhijit.dutta@fissionlabs.in>
2014-02-24 15:48:21 +0530

    Commiting folder structure

---
+++ CloudChamber/src/main/java/org/e8/whois/App.java
@@ -0,0 +1,13 @@
+package org.e8.whois;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
